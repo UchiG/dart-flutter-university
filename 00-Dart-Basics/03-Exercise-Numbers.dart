fe54ff958f45cf0f6913be8c98f08782a2c39f3d
Uchirai <uchirai.govere@gmail.com>
2023-08-14 23:07:56 +0200

    did exercise

--- 00-Dart-Basics/03-Exercise-Numbers.dart
+++ 00-Dart-Basics/03-Exercise-Numbers.dart
@@ -3,6 +3,10 @@ void main() {
 //  to a new variable where its rounded to the 
 //  nearest integer. 
 var someNum = 3.1;
+
+var roundedNum = someNum.rounded();
+print(roundedNum);
+
 }
 // BONUS QUESTION; In the cases of 0.5, how can you
 // ensure its rounded up or down?
