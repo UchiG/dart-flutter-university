c3bac2d804fb0ed95d29852783c9756df8c32749
Uchirai <uchirai.govere@gmail.com>
2023-08-16 00:44:06 +0200

    random commit

--- 00-Dart-Basics/04-Exercise-Strings.dart
+++ 00-Dart-Basics/04-Exercise-Strings.dart
@@ -2,4 +2,5 @@ void main() {
   // Given the string below, replace all the
   // "x"s with a blank string
   var errorMessage = "Uxh oxh, thexre's a bunxch of txypos";
+  print(errorMessage.replaceAll("x", " ")
 }
